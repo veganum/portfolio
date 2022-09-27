a78caaad0d9f7547279caf695df3a051737ff4a9
veganum <veganum@hotmail.com>
2022-09-27 22:33:15 +0200

    Scroll Implementation

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,12 +1,31 @@
 import { Grid, GridItem } from "@chakra-ui/react";
 import { DarkMode } from "./../darkMode/DarkMode";
+import { Link } from "react-scroll";
 
 export const Navbar = () => {
   return (
     <>
       <Grid templateColumns="repeat(3, 1fr)" gap={6} p={5}>
         <GridItem>Icon</GridItem>
-        <GridItem>Navbar</GridItem>
+        <GridItem>
+          <div className="header">
+            <Link activeClass="active" to="1" spy={true} smooth={true}>
+              Home
+            </Link>
+            <Link to="about" spy={true} smooth={true}>
+              About
+            </Link>
+            <Link to="curriculum" spy={true} smooth={true}>
+              Curriculum
+            </Link>
+            <Link to="proyects" spy={true} smooth={true}>
+              Proyects
+            </Link>
+            <Link to="footer" spy={true} smooth={true}>
+              Footer
+            </Link>
+          </div>
+        </GridItem>
         <GridItem>
           <DarkMode />
         </GridItem>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,33 +4,85 @@ import { Title } from "../components/title/Title";
 import { About } from "./about/About";
 import { Curriculum } from "./curriculum/Curriculum";
 import { Proyects } from "./proyects/Proyects";
+import { animateScroll as scroll } from "react-scroll";
+import { BiDownArrow, BiUpArrow } from "react-icons/bi";
 
 export const Home = () => {
+  const onClickMore = () => {
+    scroll.scrollMore(1000);
+  };
+
+  const onClickDown = () => {
+    scroll.scrollToBottom();
+  };
+
+  const onClickUp = () => {
+    scroll.scrollToTop();
+  };
+
+  const onClickMoreDown = () => {
+    scroll.scrollMore(950);
+  };
+
+  const onClickMoreUp = () => {
+    scroll.scrollMore(-950);
+  };
+
   return (
     <Grid>
-      <GridItem bgGradient="linear(to-l, #7928CA, #FF0080,orangered)">
-        <Navbar />
-      </GridItem>
-      <GridItem className="pagesWithoutColor">
-        <Title />
-        <img src="" alt="" />
-      </GridItem>
-      <GridItem
-        className="pagesColor"
-        bgGradient="linear(to-l, #7928CA, #FF0080,orangered)"
-      >
-        <About />
-      </GridItem>
-      <GridItem className="pagesWithoutColor">
-        <Curriculum />
-      </GridItem>
-      <GridItem
-        className="pagesColor"
-        bgGradient="linear(to-l, #7928CA, #FF0080,orangered)"
-      >
-        <Proyects />
-      </GridItem>
-      <GridItem className="pagesWithoutColor">Footbar</GridItem>
+      <div className="navigationMenu">
+        <button className="up" onClick={onClickUp}>
+          <BiUpArrow /> {/* Ir Arriba */}
+        </button>
+        <button className="more-up" onClick={onClickMoreUp}>
+          <BiUpArrow />
+          +1{/* Subir */}
+        </button>
+        <button className="more-down" onClick={onClickMoreDown}>
+          <BiDownArrow />
+          -1{/* Bajar */}
+        </button>
+        <button className="down" onClick={onClickDown}>
+          <BiDownArrow />
+          {/* Ir Abajo */}
+        </button>
+      </div>
+
+      <div id="home" className="section" onClick={onClickMore}>
+        <GridItem bgGradient="linear(to-l, #7928CA, #FF0080,orangered)">
+          <Navbar />
+        </GridItem>
+      </div>
+      <div id="home" className="section" onClick={onClickMore}>
+        <GridItem className="pagesWithoutColor">
+          <Title />
+          <img src="" alt="" />
+        </GridItem>
+      </div>
+      <div id="about" className="section" onClick={onClickMore}>
+        <GridItem
+          className="pagesColor"
+          bgGradient="linear(to-l, #7928CA, #FF0080,orangered)"
+        >
+          <About />
+        </GridItem>
+      </div>
+      <div id="curriculum" className="section" onClick={onClickMore}>
+        <GridItem className="pagesWithoutColor">
+          <Curriculum />
+        </GridItem>
+      </div>
+      <div id="proyects" className="section" onClick={onClickMore}>
+        <GridItem
+          className="pagesColor"
+          bgGradient="linear(to-l, #7928CA, #FF0080,orangered)"
+        >
+          <Proyects />
+        </GridItem>
+      </div>
+      <div id="footer" className="section" onClick={onClickMore}>
+        <GridItem className="pagesWithoutColor">Footbar</GridItem>
+      </div>
     </Grid>
   );
 };
